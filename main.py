5bfee763f95389be42c4af1063c095f541752470
Fred Morrison <frederick.morrison@outlook.com>
2025-04-01 13:02:57 -0600

    define and use Person class

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+from person import Person
 
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
+def main():
+    p = Person('Hansel', 'Bobbie')
+    print(p)
 
 if __name__ == '__main__':
     print(f'Python version: {get_python_version()}')
+    main()
